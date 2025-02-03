9a32a10519c4bb3078f1d8b676082316279edc9a
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-02-03 13:38:28 -0500

    lex and Token class can track user program location

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -12,21 +12,36 @@ Globals:
         // Grab the "raw" source code.
         var sourceCode = document.getElementById("taSourceCode").value;
         // Trim the leading and trailing spaces.
-        sourceCode = trim(sourceCode);
-        
-        //loop through text to find tokens
-        while (tokenIndex < sourceCode.length){
-            currentChar = sourceCode[tokenIndex];
+        //sourceCode = trim(sourceCode);
 
-            tokenLine = 1;
-            //create token object
-            putMessage("before - token");
-            newToken = new Token(currentChar, tokenLine, tokenIndex);
-            putMessage("made - token");
-            putMessage("TOKEN [" + newToken.name + "]");
-            tokenIndex++;
+        //where in user code are we
+        sourceIndex = 0;
+        userLineIndex = 1;  //start at 1
+        userCharIndex = 1;  //start at 1
+
+        //loop through text to find tokens
+        while (sourceIndex < sourceCode.length){
+            currentChar = sourceCode[sourceIndex];
+
+            //create Token object
+            newToken = new Token(currentChar, userLineIndex, userCharIndex);
+            //putMessage("TOKEN [" + newToken.name + "]");
+
+            //Move index
+            //new user line
+            if (currentChar === "\n") {
+                userCharIndex = 1;  //start at 1
+                userLineIndex ++;
+            }
+            //same user line
+            else {
+                userCharIndex ++;
+            }
+            //next source char
+            sourceIndex ++;
         }
-        putMessage("EOP!");
+
+        //putMessage("EOP!");
 
         //return a list of tokens
         return sourceCode;
--- scripts/token.js
+++ scripts/token.js
@@ -1,7 +1,7 @@
 /* token class */
 
     
-export class Token {
+class Token {
 
     constructor(name, startLine, startIndex) { 
                     
@@ -10,88 +10,19 @@ export class Token {
         this.startLine = startLine;
         this.startIndex = startIndex;
 
-        //tell
-        putMessage("constructing token");
+        //new token constructed!
+        this.tid = tokenIndex; //token ID
+        tokenIndex ++;
+
+        //debug
+        putMessage("Token "+this.tid+" ["+name+", "+startLine+":"+startIndex+"]");
     }
 
     //setters
+    /*
     setName(inputName) {
         this.name = inputName;
     }
-    /*
-    public setState(state: string) {
-        this.state = state;
-        _Kernel.krnTrace('PID ' + this.pid + ' set to ' + this.state);
-        Control.updatePCBDisplay(this);
-    }
-    public setSegment(segment: number) {
-        
-        //for storing in memory...
-        if (segment == STORE_ON_DISK) {
-            this.segment = ERROR_CODE;
-
-            //set the base and limit
-            var base = this.getBase();      //ERROR -> ERROR
-            var limit = this.getLimit();    //ERROR -> ERROR
-
-            //location
-            this.location = "Disk";
-        }
-
-        //for deleting...
-        else if (segment == ERROR_CODE) {
-            this.segment = ERROR_CODE;
-            this.base = ERROR_CODE;
-            this.limit = ERROR_CODE;
-
-            //location
-            this.location = "---";
-        }
-
-        //for storing on disk...
-        else {
-            this.segment = segment;
-
-            //set segment in eyes of memory manager
-            _MemoryManager.segmentList[segment] = this;
-
-            //also sets the base and limit
-            var base = this.getBase();      //0->0x000 1->0x100 2->0x200
-            var limit = this.getLimit();    //0->0x0FF 1->0x1FF 2->0x2FF
-
-            //location
-            this.location = "Memory";
-        }
-        
-        //update display
-        Control.updatePCBDisplay(this);
-    }
-
-    //getters
-    public getBase() {
-        //no segment = no base
-        if (this.segment == ERROR_CODE) {
-            this.base = ERROR_CODE;
-            return this.base;
-        }
-        this.base = SEGMENT_SIZE * this.segment;
-        return this.base;
-    }
-    public getLimit() {
-        //no segment = no limit
-        if (this.segment == ERROR_CODE) {
-            this.limit = ERROR_CODE;
-            return this.limit;
-        }
-        this.base = this.base + SEGMENT_SIZE - 0x01;
-        return this.base;
-    }
-    public getState() {
-        return this.state;
-    }
-    public getSegment() {
-        return this.segment;
-    }
-        */
+    */
 }
     
