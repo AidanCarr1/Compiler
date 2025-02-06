6de74185a72fbfbc46301f98eaaee601e1c1dd0a
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-02-06 18:09:19 -0500

    working with some keyword arrays, also watching TV so didnt get much done

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -6,12 +6,12 @@ Globals:
     currentToken = ' ';
     errorCount = 0;  
 
-Langauge Order:
-    keywords
-    id
-    symbol
-    digit
-    char
+Language Order:
+    keywords:   int string boolean while if false true print
+    id:         a b c d e f g h i j k l m n o p q r s t u v w x y z
+    symbol:     == != " " ( ) { } /*
+    digit:      0 1 2 3 4 5 6 7 8 9 
+    char:       a b c d e f g h i j k l m n o p q r s t u v w x y z [space]
 
 Keywords:
     { } print ( ) 
@@ -23,13 +23,24 @@ Keywords:
     == !=
     false true
     +
-    /* */
+    /* 
+*/
 
 
-function lex()
-{
+//Arrays
+keywords = ["int", "string", "boolean", "while", "if", "false", "true", "print"];
+ids =      ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+symbols =  ["==", "!=", "\"", "(", ")", "{", "}", "/*", "*/"];
+digits =   ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+chars =    [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",  
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+function lex() {
     // Grab the "raw" source code.
     var sourceCode = document.getElementById("taSourceCode").value;
+    
     // Trim the leading and trailing spaces.
     //sourceCode = trim(sourceCode);
 
@@ -38,14 +49,17 @@ function lex()
     userLineIndex = 1;  //start at 1
     userCharIndex = 1;  //start at 1
 
+    //token strings
+    checkingToken = "";
+
     //loop through text to find tokens
     while (sourceIndex < sourceCode.length){
-        currentChar = sourceCode[sourceIndex];
-        
 
-        //create Token object
-        newToken = new Token(currentChar, userLineIndex, userCharIndex);
-        //putMessage("TOKEN [" + newToken.name + "]");
+        //look at the next character for a new token
+        currentChar = sourceCode[sourceIndex];
+        checkingToken += currentChar;
+
+        
 
         //Move index
         //new user line
@@ -59,6 +73,10 @@ function lex()
         }
         //next source char
         sourceIndex ++;
+
+        //create Token object
+        newToken = new Token(currentChar, userLineIndex, userCharIndex);
+        //putMessage("TOKEN [" + newToken.name + "]");
     }
 
     //return a list of tokens
