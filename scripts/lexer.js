ccabd3469bdb2a269d682c6997f2f5827740550d
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-02-06 16:34:52 -0500

    Organizing and writing notes for the grammar

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -5,45 +5,63 @@ Globals:
     tokenIndex = 0;
     currentToken = ' ';
     errorCount = 0;  
-*/
 
-    function lex()
-    {
-        // Grab the "raw" source code.
-        var sourceCode = document.getElementById("taSourceCode").value;
-        // Trim the leading and trailing spaces.
-        //sourceCode = trim(sourceCode);
+Langauge Order:
+    keywords
+    id
+    symbol
+    digit
+    char
 
-        //where in user code are we
-        sourceIndex = 0;
-        userLineIndex = 1;  //start at 1
-        userCharIndex = 1;  //start at 1
+Keywords:
+    { } print ( ) 
+    =
+    while if
+    " " ( ) 
+    int string boolean
+    char:   a b c d e f ... z [space]
+    == !=
+    false true
+    +
+    /* */
 
-        //loop through text to find tokens
-        while (sourceIndex < sourceCode.length){
-            currentChar = sourceCode[sourceIndex];
 
-            //create Token object
-            newToken = new Token(currentChar, userLineIndex, userCharIndex);
-            //putMessage("TOKEN [" + newToken.name + "]");
+function lex()
+{
+    // Grab the "raw" source code.
+    var sourceCode = document.getElementById("taSourceCode").value;
+    // Trim the leading and trailing spaces.
+    //sourceCode = trim(sourceCode);
 
-            //Move index
-            //new user line
-            if (currentChar === "\n") {
-                userCharIndex = 1;  //start at 1
-                userLineIndex ++;
-            }
-            //same user line
-            else {
-                userCharIndex ++;
-            }
-            //next source char
-            sourceIndex ++;
+    //where in user code are we
+    sourceIndex = 0;
+    userLineIndex = 1;  //start at 1
+    userCharIndex = 1;  //start at 1
+
+    //loop through text to find tokens
+    while (sourceIndex < sourceCode.length){
+        currentChar = sourceCode[sourceIndex];
+        
+
+        //create Token object
+        newToken = new Token(currentChar, userLineIndex, userCharIndex);
+        //putMessage("TOKEN [" + newToken.name + "]");
+
+        //Move index
+        //new user line
+        if (currentChar === "\n") {
+            userCharIndex = 1;  //start at 1
+            userLineIndex ++;
         }
-
-        //putMessage("EOP!");
-
-        //return a list of tokens
-        return sourceCode;
+        //same user line
+        else {
+            userCharIndex ++;
+        }
+        //next source char
+        sourceIndex ++;
     }
+
+    //return a list of tokens
+    return sourceCode;
+}
 
