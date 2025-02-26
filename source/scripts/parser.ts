92b00023b19b84e476efe9bb1f9b2c326d0a1af5
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-02-26 14:32:33 -0500

    halfway thru first layer of the parser.

--- source/scripts/parser.ts
+++ source/scripts/parser.ts
@@ -9,51 +9,75 @@ namespace Compiler {
 
 
         public static parseProgram() {
-            
+            this.parseBlock();
+            this.match("EOP");
         }
 
         public static parseBlock() {
-
+            this.match("OPEN BRACE");
+            this.parseStatementList();
+            this.match("CLOSE BRACE");
         }
 
+        //INCOMPLETE
         public static parseStatementList() {
-            
+            //if (token in print, assignment, vardec...)
+                this.parseStatement();
+                this.parseStatementList();
+            //else
+                //do nothing
+                //Final statement
         }
 
+        //INCOMPLETE
         public static parseStatement() {
             
         }
 
         public static parsePrintStatement() {
-            
+            this.match("PRINT");
+            this.match("OPEN PARENTHESIS");
+            this.parseExpr();
+            this.match("CLOSE PARENTHESIS");
         }
 
         public static parseAssignmentStatement() {
-            
+            this.match("ID");
+            this.match("ASSIGNMENT");
+            this.parseExpr();
         }
         
         public static parseVarDecl() {
-            
+            this.parseType();
+            this.parseId();
         }
 
         public static parseWhileStatement() {
-            
+            this.match("WHILE");
+            this.parseBooleanExpr();
+            this.parseBlock();
         }
 
         public static parseIfStatement() {
-            
+            this.match("IF");
+            this.parseBooleanExpr();
+            this.parseBlock();
         }
 
+        //INCOMPLETE
         public static parseExpr() {
             
         }
 
+        //INCOMPLETE
         public static parseIntExpr() {
             
         }
 
         public static parseStringExpr() {
-            
+            this.match("QUOTATION");
+            this.parseCharList();
+            this.match("QUOTATION");
         }
 
         public static parseBooleanExpr() {
