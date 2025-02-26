bd7622fd4d100b93fd410e6de236d5c73df6ab04
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-02-26 17:29:01 -0500

    Tree and Node structures are a work in progress

--- source/scripts/node.ts
+++ source/scripts/node.ts
@@ -1,9 +1,23 @@
-/*  Node clas
-    with tree class
+/*  Node class
+    With the tree class for CST
 */
 
 namespace Compiler {
     export class Node {
 
+        constructor(public name?, 
+            public parent?, 
+            public children?) { 
+
+            //Node knows itself, parent, children
+            this.name = name;
+            this.parent = parent;       //maybe make these null
+            this.children = [];
+        }
+
+        //Add another to the list of children
+        public addChild(childNode) {
+            this.children.push(childNode);
+        }
     }
 }
--- source/scripts/tree.ts
+++ source/scripts/tree.ts
@@ -1,9 +1,37 @@
 /*  Tree class
-    for CST
+    For the CST
 */
 
 namespace Compiler {
     export class Tree {
 
+        constructor(public root?,
+                    public current? ) { 
+
+            //Simple tree structure, remember first and recent
+            this.root = null;
+            this.current = null;
+        }
+
+        //Add new Node into the tree somewhere
+        public addNode(kind, label) {
+            var newNode = new Node();
+            newNode.name = label;
+
+            //First node, root node
+            if (this.root == null) {
+                this.root = newNode;
+            }
+
+            //WORK IN PROGRESS, copied pseudocode
+            else {
+                newNode.parent = this.current;
+                newNode.parent.addChild(newNode);
+            }
+
+            if (kind == "branch") {
+                this.current = newNode;
+            }
+        }
     }
 }
