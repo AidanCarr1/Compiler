b3ec1da8847e0495e47cad49cbddf8a0599ab620
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-03-03 09:31:33 -0500

    Added parse print statements

--- dist/scripts/parser.js
+++ dist/scripts/parser.js
@@ -8,16 +8,19 @@ var Compiler;
 (function (Compiler) {
     class Parser {
         static parseProgram() {
+            Compiler.Control.putMessage("PARSE: parseProgram()");
             this.parseBlock();
             this.match("EOP");
         }
         static parseBlock() {
+            Compiler.Control.putMessage("PARSE: parseBlock()");
             this.match("OPEN BRACE");
             this.parseStatementList();
             this.match("CLOSE BRACE");
         }
         //INCOMPLETE
         static parseStatementList() {
+            Compiler.Control.putMessage("PARSE: parseStatementList()");
             //if (token in print, assignment, vardec...)
             this.parseStatement();
             this.parseStatementList();
@@ -27,68 +30,87 @@ var Compiler;
         }
         //INCOMPLETE
         static parseStatement() {
+            Compiler.Control.putMessage("PARSE: parseStatement()");
         }
         static parsePrintStatement() {
+            Compiler.Control.putMessage("PARSE: parsePrintStatement()");
             this.match("PRINT");
             this.match("OPEN PARENTHESIS");
             this.parseExpr();
             this.match("CLOSE PARENTHESIS");
         }
         static parseAssignmentStatement() {
+            Compiler.Control.putMessage("PARSE: parseAssignmentStatement()");
             this.match("ID");
             this.match("ASSIGNMENT");
             this.parseExpr();
         }
         static parseVarDecl() {
+            Compiler.Control.putMessage("PARSE: parseVarDecl()");
             this.parseType();
             this.parseId();
         }
         static parseWhileStatement() {
+            Compiler.Control.putMessage("PARSE: parseWhileStatement()");
             this.match("WHILE");
             this.parseBooleanExpr();
             this.parseBlock();
         }
         static parseIfStatement() {
+            Compiler.Control.putMessage("PARSE: parseIfStatement()");
             this.match("IF");
             this.parseBooleanExpr();
             this.parseBlock();
         }
         //INCOMPLETE
         static parseExpr() {
+            Compiler.Control.putMessage("PARSE: parseExpr()");
         }
         //INCOMPLETE
         static parseIntExpr() {
+            Compiler.Control.putMessage("PARSE: parseIntExpr()");
         }
         static parseStringExpr() {
+            Compiler.Control.putMessage("PARSE: parseStringExpr()");
             this.match("QUOTATION");
             this.parseCharList();
             this.match("QUOTATION");
         }
         static parseBooleanExpr() {
+            Compiler.Control.putMessage("PARSE: parseBooleanExpr()");
         }
         static parseId() {
+            Compiler.Control.putMessage("PARSE: parseId()");
         }
         static parseCharList() {
+            Compiler.Control.putMessage("PARSE: parseCharList()");
         }
         static parseType() {
+            Compiler.Control.putMessage("PARSE: parseType()");
             this.match("VARIABLE TYPE");
         }
         //INCOMPLETE - check with space and char dictionary
         static parseChar() {
+            Compiler.Control.putMessage("PARSE: parseChar()");
             this.match("CHAR");
         }
         //INCOMPLETE - check with space and char dictionary
         static parseSpace() {
+            Compiler.Control.putMessage("PARSE: parseSpace()");
             this.match("SPACE CHAR");
         }
         static parseDigit() {
+            Compiler.Control.putMessage("PARSE: parseDigit()");
             this.match("DIGIT");
         }
         static parseBoolOp() {
+            Compiler.Control.putMessage("PARSE: parseBoolOp()");
         }
         static parseBoolVal() {
+            Compiler.Control.putMessage("PARSE: parseBoolVal()");
         }
         static parseIntOp() {
+            Compiler.Control.putMessage("PARSE: parseIntOp()");
             this.match("ADDITION");
         }
         static match(str, strList) {
--- source/scripts/parser.ts
+++ source/scripts/parser.ts
@@ -10,11 +10,15 @@ namespace Compiler {
 
 
         public static parseProgram() {
+            Control.putMessage("PARSE: parseProgram()");
+
             this.parseBlock();
             this.match("EOP");
         }
 
         public static parseBlock() {
+            Control.putMessage("PARSE: parseBlock()");
+            
             this.match("OPEN BRACE");
             this.parseStatementList();
             this.match("CLOSE BRACE");
@@ -22,6 +26,8 @@ namespace Compiler {
 
         //INCOMPLETE
         public static parseStatementList() {
+            Control.putMessage("PARSE: parseStatementList()");
+            
             //if (token in print, assignment, vardec...)
                 this.parseStatement();
                 this.parseStatementList();
@@ -32,10 +38,13 @@ namespace Compiler {
 
         //INCOMPLETE
         public static parseStatement() {
+            Control.putMessage("PARSE: parseStatement()");
             
         }
 
         public static parsePrintStatement() {
+            Control.putMessage("PARSE: parsePrintStatement()");
+            
             this.match("PRINT");
             this.match("OPEN PARENTHESIS");
             this.parseExpr();
@@ -43,23 +52,31 @@ namespace Compiler {
         }
 
         public static parseAssignmentStatement() {
+            Control.putMessage("PARSE: parseAssignmentStatement()");
+            
             this.match("ID");
             this.match("ASSIGNMENT");
             this.parseExpr();
         }
         
         public static parseVarDecl() {
+            Control.putMessage("PARSE: parseVarDecl()");
+            
             this.parseType();
             this.parseId();
         }
 
         public static parseWhileStatement() {
+            Control.putMessage("PARSE: parseWhileStatement()");
+            
             this.match("WHILE");
             this.parseBooleanExpr();
             this.parseBlock();
         }
 
         public static parseIfStatement() {
+            Control.putMessage("PARSE: parseIfStatement()");
+            
             this.match("IF");
             this.parseBooleanExpr();
             this.parseBlock();
@@ -67,59 +84,83 @@ namespace Compiler {
 
         //INCOMPLETE
         public static parseExpr() {
+            Control.putMessage("PARSE: parseExpr()");
+            
             
         }
 
         //INCOMPLETE
         public static parseIntExpr() {
+            Control.putMessage("PARSE: parseIntExpr()");
+            
             
         }
 
         public static parseStringExpr() {
+            Control.putMessage("PARSE: parseStringExpr()");
+            
             this.match("QUOTATION");
             this.parseCharList();
             this.match("QUOTATION");
         }
 
         public static parseBooleanExpr() {
+            Control.putMessage("PARSE: parseBooleanExpr()");
             
         }
 
         public static parseId() {
+            Control.putMessage("PARSE: parseId()");
+            
             
         }
 
         public static parseCharList() {
+            Control.putMessage("PARSE: parseCharList()");
             
         }
 
         public static parseType() {
+            Control.putMessage("PARSE: parseType()");
+            
             this.match("VARIABLE TYPE");
         }
 
         //INCOMPLETE - check with space and char dictionary
         public static parseChar() {
+            Control.putMessage("PARSE: parseChar()");
+            
             this.match("CHAR");
         }
 
         //INCOMPLETE - check with space and char dictionary
         public static parseSpace() {
+            Control.putMessage("PARSE: parseSpace()");
+            
             this.match("SPACE CHAR");
         }
 
         public static parseDigit() {
+            Control.putMessage("PARSE: parseDigit()");
+            
             this.match("DIGIT");
         }
 
         public static parseBoolOp() {
+            Control.putMessage("PARSE: parseBoolOp()");
+            
             
         }
 
         public static parseBoolVal() {
+            Control.putMessage("PARSE: parseBoolVal()");
+            
             
         }
 
         public static parseIntOp() {
+            Control.putMessage("PARSE: parseIntOp()");
+            
             this.match("ADDITION");
         }
 
